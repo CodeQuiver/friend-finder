2bb0058a5f63805d2f97b5df154a1f2289fee3d2
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-06-12 20:05:58 -0400

    tested survey successfully including correct push to array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,6 +21,9 @@ module.exports = function(app) {
     // ...the JSON is pushed to the appropriate JavaScript array
     app.post("/api/friends", function(req, res) {
         var currentUser = req.body; //set name for user who entered request for clarity
+
+        
+        friendData.push(currentUser);
         
         // reference the entries in friend array using friendData[i] in a for loop
 
@@ -31,12 +34,12 @@ module.exports = function(app) {
         for (let i = 0; i < friendData.length; i++) {
             var compareFriend = friendData[i].scores;
             var totalDiff = 0;
+
            for (let j = 0; j < compareFriend.length; j++) {
                 var userScore = currentUser.scores[j];
                 var friendScore = compareFriend[j];
                 totalDiff += Math.abs(userScore - friendScore);
-                
-           }
+            }
 
            // totalDiff is the sum of differences between friendData[i].scores and currentUser.scores
            if (bestFriendScore >= totalDiff) {
@@ -46,11 +49,12 @@ module.exports = function(app) {
         }
 
         var bestFriend = friendData[bestFriendIndex];
-        res.json(friendData);
+        res.json(bestFriend);
+        // this should send the bestFriend entry back to the page as a response, which will plug into the "data" argument
 
 
         // push current user data to frienddata array in friends.js file- make sure this happens after the above function ends
-        friendData.push(currentUser);
+        // friendData.push(currentUser);
 
         // log entire friends array to check it's correct
         // console.log(res.json(friendData));
